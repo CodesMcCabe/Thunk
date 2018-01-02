3c3bd0e569e8e99ffa824e98370bcfc1ba244b11
Roots2D <cmccabe524@gmail.com>
2018-01-02 14:44:42 -0500

    Add reducers and store, setup default state

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,44 @@
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+import * as SessionApiUtil from '../util/session_api_util';
+
+export const login = (user) => dispatch => {
+  return(
+    SessionApiUtil.login(user).then(currentUser =>
+      dispatch(receiveCurrentUser(currentUser))
+    ), err => (
+      dispatch(receiveErrors(err.responseJSON))
+    )
+  );
+};
+
+export const logout = () => dispatch => {
+  return(
+    SessionApiUtil.logout().then(user =>
+      dispatch(receiveCurrentUser(null)))
+  );
+};
+
+export const signup = (user) => dispatch => {
+  return(
+    SessionApiUtil.signup(user).then(currentUser =>
+      dispatch(receiveCurrentUser(currentUser))
+    ), err => (
+      dispatch(receiveErrors(err.responseJSON))
+    )
+  );
+};
+
+const receiveCurrentUser = (currentUser) => {
+  return({
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+  });
+};
+
+const receiveErrors = (errors) => {
+  return({
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+  });
+};
--- frontend/thunk.jsx
+++ frontend/thunk.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import configureStore from './store/store';
+import configureStore from './store/store';
 import ReactDOM from 'react-dom';
 import {signup, login, logout} from './util/session_api_util';
 
@@ -9,5 +9,9 @@ window.logout = logout;
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
+  const store = configureStore();
   ReactDOM.render(<h1>Welcome to Thunk</h1>, root);
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 });
