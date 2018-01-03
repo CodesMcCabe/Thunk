2e23bc56a387fe83c5cece9bc96747516fb6c114
Roots2D <cmccabe524@gmail.com>
2018-01-02 19:51:01 -0500

    Add custom AuthRoute, user login functionality working

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
+import SessionFormContainer from './session_form/session_form_container';
+import { AuthRoute } from '../util/route_util';
 
 const App = () => (
   <div>
@@ -7,6 +10,9 @@ const App = () => (
       <h1>Thunk</h1>
       <GreetingContainer />
     </header>
+
+    <AuthRoute path="/login" component={SessionFormContainer} />
+    <AuthRoute path="/signup" component={SessionFormContainer} />
   </div>
 );
 
--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
 
 class SessionForm extends React.Component {
   constructor(props) {
@@ -11,8 +12,73 @@ class SessionForm extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.loggedIn) {
+      this.props.history.push('/');
+    }
+  }
+
   handleSubmit(e) {
     e.preventDefault();
-    // this.props.
+    const user = Object.assign({}, this.state);
+    this.props.processForm({user});
+  }
+
+  navLink() {
+    if (this.props.formType === 'login') {
+      return <Link to="/signup">Sign Up</Link>;
+    } else {
+      return <Link to="/login">Log In</Link>;
+    }
+  }
+
+  renderErrors() {
+    return(
+      <ul>
+        {this.props.errors.map((error, i) => (
+          <li key={`error-${i}`}>
+            {error}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  update(field) {
+    return (e) => {
+      this.setState({ [field]: e.target.value });
+    };
+  }
+
+  render() {
+    return(
+      <div>
+        <br/>
+        <form onSubmit={this.handleSubmit}>
+          Welcome to Thunk!
+          <br/>
+          Please {this.props.formType} or {this.navLink()}
+          {this.renderErrors()}
+          <div>
+            <br/>
+            <label>Username:
+              <input type="text"
+                value={this.state.username}
+                onChange={this.update('username')}/>
+            </label>
+            <br/>
+            <label>Password:
+              <input type="password"
+                value={this.state.password}
+                onChange={this.update('password')}/>
+            </label>
+
+            <button>Submit</button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
+
+export default withRouter(SessionForm);
