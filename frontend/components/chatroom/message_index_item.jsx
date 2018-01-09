5d21853ea5f5465adb1fffd960cc73ac5f23666c
Roots2D <cmccabe524@gmail.com>
2018-01-09 17:22:57 -0500

    Add message CSS, done besides date/image

--- frontend/components/chatroom/message_index_item.jsx
+++ frontend/components/chatroom/message_index_item.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
 const MessageIndexItem = ({ user, message }) => {
-  // STYLE AT THE UL LEVEL FOR FLEX USERNAME AND TIME INLINE
-  // MESSAGE BELOW
-  // AVATAR TO LEFT
+
+  // ADD PROFILE PICTURE IN IMAGE SPOT
+  // UPDATE USER MODEL TO ACCEPT A PROFILE PIC
   return (
     <li className="message_container">
         <div className="message_avatar">Image</div>
         <div className="message_data_container">
           <div className="message_header">
             <p className="message_username">{user.username}</p>
-            <p className="message_time">{message.created_at}</p>
+            <p className="message_time">
+              {message.created_at}
+            </p>
           </div>
           <div className="message_content">
             <p>{message.content}</p>
